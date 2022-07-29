0ac569dc7a29c8c5af7b114fd6cad9606e969cd0
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-07-29 22:05:30 +0900

    test

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+//インストールなしでOK
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("connection");
+  socket.on("chat message", (msg) => {
+    // console.log("メッセージ：" + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("Server running");
+});
